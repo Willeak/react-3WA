0ccb869f9d89eae66632de425bb9f206dcb52638
BuildTools <unconfigured@null.spigotmc.org>
2023-11-06 12:01:54 +0100

    upload exo2

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-const helloWorld = React.createElement('h1', {}, 'Hello world!');
+const helloWorld = React.createElement("h1", {}, "Hello world!");
 
+const text = <h2>hello</h2>;
 
-// mauvaise pratique
-
-
-//ReactDOM.render(helloWorld, document.body);
-
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+ReactDOM.render(text, document.querySelector("#app"));
