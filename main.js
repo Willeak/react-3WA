5b44e350071261368c39fdb435bb8dc6c6464b98
BuildTools <unconfigured@null.spigotmc.org>
2023-11-07 17:34:49 +0100

    upload exo9

--- main.js
+++ main.js
@@ -1,37 +1,147 @@
-function App(props) {
-    React.useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(usersObject => setUsers(usersObject))
-    }, [])
+function NameForm() {
+  const [value, setValue] = React.useState("");
 
-    const [users, setUsers] = React.useState([]);
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleSubmit = (e) => {
+    alert("Le nom a été soumis : " + value);
+    e.preventDefault();
+  };
 
-
-
-    return(
-        <React.Fragment>
-        {
-            users.map((u) =>(
-                <UserCard key={u.id} user={u}/>
-            ))
-        }
-        </React.Fragment>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nom
+        <br />
+        <input type="text" value={value} onChange={handleChange} />
+      </label>
+      <input type="submit" value="Envoyer" />
+    </form>
+  );
 }
 
-function UserCard({user}) {
-    
+function EssayForm() {
+  const [value2, setValue2] = React.useState(
+    "Écrivez un essai à propos de votre élément du DOM préféré"
+  );
 
-    return (
-        <ul className="user-card">
-            <li>{user.name}</li>
-            <li>{user.email}</li>
-            <li>{user.company.name}</li>
-            <li>{user.phone}</li>
-            <li>{user.website}</li>
-        </ul>
-    )
+  const handleChange = (event) => {
+    setValue2(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleSubmit = (e) => {
+    alert("Le nom a été soumis : " + value2);
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Essay
+        <br />
+        <textarea value={value2} onChange={handleChange} />
+      </label>
+      <input type="submit" value="Envoyer" />
+    </form>
+  );
 }
 
-ReactDOM.render(<App/>, document.querySelector('#app'))
+function FlavorForm() {
+  const [value3, setValue3] = React.useState("");
+
+  const handleChange = (event) => {
+    setValue3(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    alert("Le nom a été soumis : " + value3);
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Choisissez votre parfum favori
+        <br />
+        <select value={value3} onChange={handleChange}>
+          <option value="grapefruit">Pamplemousse</option>
+          <option value="lime">Citron vert</option>
+          <option value="coconut">Noix de coco</option>
+          <option value="mango">Mangue</option>
+        </select>
+      </label>
+      <input type="submit" value="Envoyer" />
+    </form>
+  );
+}
+
+function AllForm() {
+  const [value, setValue] = React.useState("");
+  const [value2, setValue2] = React.useState(
+    "Écrivez un essai à propos de votre élément du DOM préféré"
+  );
+  const [value3, setValue3] = React.useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleChange2 = (event) => {
+    setValue2(event.target.value);
+
+    console.log(event.target.value);
+  };
+  const handleChange3 = (event) => {
+    setValue3(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleSubmit = (e) => {
+    alert(
+      `Le nom a été soumis : ${value}\nL'essay' a été soumis : ${value2}\nLe parfum a été soumis : ${value3}`
+    );
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nom
+        <br />
+        <input type="text" value={value} onChange={handleChange} />
+      </label>
+
+      <label>
+        Essay
+        <br />
+        <textarea value={value2} onChange={handleChange2} />
+      </label>
+      <label>
+        Choisissez votre parfum favori
+        <br />
+        <select value={value3} onChange={handleChange3}>
+          <option value="grapefruit">Pamplemousse</option>
+          <option value="lime">Citron vert</option>
+          <option value="coconut">Noix de coco</option>
+          <option value="mango">Mangue</option>
+        </select>
+      </label>
+      <input type="submit" value="Envoyer" />
+    </form>
+  );
+}
+
+ReactDOM.render(
+  <React.Fragment>
+    <div className="row">
+      <NameForm />
+      <EssayForm />
+      <FlavorForm />
+    </div>
+
+    <AllForm />
+  </React.Fragment>,
+  document.querySelector("#app")
+);
