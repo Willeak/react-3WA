ea40a0e61be634f28729dc9029d15816eaf64bf3
BuildTools <unconfigured@null.spigotmc.org>
2023-11-07 11:35:14 +0100

    upload exo5

--- main.js
+++ main.js
@@ -1,37 +1,21 @@
-const firstName = 'r0ulito';
-const lastName = 'formateur';
+function tick() {
+  ReactDOM.render(<Clock date={new Date()} />, document.getElementById("app"));
+}
+setInterval(tick, 1000);
 
-function FirstName(props) {
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    /*
-    // Solution avec bonus
-    const formatFirstName = (firstName) => {
-        return firstName[0].toUpperCase() + firstName.substr(1);
-    }
-
-    return <span>{formatFirstName(props.text)}</span>
-    */
-
-    // Solution sans bonus
-    return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
+  render() {
+    return (
+      <div>
+        <h1>Bonjour, monde !</h1>
+        <h2>Il est {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-function LastName(props) {
-
-    /*
-    // Solution avec bonus
-    const formatLastName = (lastName) => {
-        return lastName.toUpperCase();
-    }
-
-    return <span>{formatLastName(props.text)}</span>
-    */
-
-    // Solution sans bonus
-    return <span className="red-text">{props.text.toUpperCase()}</span>
-
-}
-
-const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
-
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+ReactDOM.render(<Clock />, document.querySelector("#app"));
